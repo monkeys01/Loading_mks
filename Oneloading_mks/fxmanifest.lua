01e1ae78b0f5fdc5908f1e0ce5e1bb0dc21af4a5
MKS Studio <99986762+monkeys01@users.noreply.github.com>
2025-01-31 08:45:02 +0100

    Add files via upload

---
+++ Oneloading_mks/fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'VotrePseudo'
+description 'Loading Screen pour serveur FiveM avec son et curseur personnalisé'
+version '1.1.0'
+
+files {
+    'index.html',
+    'style.css',
+    'video/video.mp4', -- Vidéo
+    'cursor/custom-cursor.png', -- Curseur normal
+    'cursor/custom-cursor-hover.png' -- Curseur hover
+}
+
+loadscreen 'index.html'
